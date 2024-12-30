0779fb3e4d1f39085e871d244a06c3b0a3ce747f
Galsan81 <galsmizhit@gmail.com>
2024-12-30 08:55:18 +0800

    Add files via upload

---
+++ module3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    new_string = string.lower()
+    for i in list_to_search:
+        if new_string == i.lower():
+            flag = True
+        else: flag = False
+
+    return flag
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
